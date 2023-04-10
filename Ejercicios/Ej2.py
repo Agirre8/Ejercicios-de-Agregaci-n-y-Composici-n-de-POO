b27632afb7604d01053aaca0a61cc3047a25ef9d
Agirre8 <inagi30@gmail.com>
2023-04-10 17:15:32 +0200

    ej1 acabado

---
+++ Ejercicios/Ej2.py
@@ -0,0 +1,26 @@
+class Yin: pass 
+class Yang: 
+    def __del__(self): 
+        print("Yang destruido") 
+
+
+yin = Yin() 
+yang = Yang() 
+
+
+#creamos un atributo yang en el objeto yin, y le asigna el valor del omjeto yang
+yin.yang = yang 
+ 
+print(yang) 
+print(yang is yin.yang) 
+
+
+#espera a que acabe el programa ya que la variable yang todavía esta en uso, cuando acaba el programa elimina la variable yang, por eso el mensaje sale al final por mucho que este antes llamado el método
+
+yang.__del__()
+#llamando al metodo explicitamente podemos hacer que imprima "Yang destruido" antes que "?", eso no quita que al final del programa se vuelva a imrimir, ya que esa es la funcion espedial __del__
+#>>>  Yang destruido
+#>>>  ?
+#>>>  Yang destruido
+
+print("?") 
